dce7cd6c55c4b07a0cf570967e7b401249b1fa8c
foisaluddin400 <foisalrk2@gmail.com>
2025-05-11 13:44:53 +0600

    ewra

--- src/page/redux/api/baseApi.jsx
+++ src/page/redux/api/baseApi.jsx
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://10.0.60.129:3000",
+  baseUrl: "https://api.cathysjewelry.net",
   // prepareHeaders: (headers) => {
   //   const token = JSON.parse(localStorage.getItem("accessToken"));
   //   if (token) {
@@ -26,5 +26,5 @@ export const baseApi = createApi({
   endpoints: () => ({}),
 });
 
-export const imageUrl = "http://10.0.60.129:3000";
+export const imageUrl = "https://api.cathysjewelry.net";
 // asdfsf
